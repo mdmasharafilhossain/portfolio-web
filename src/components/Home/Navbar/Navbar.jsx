4995a05defe8034ea38cdf3d8cb0c828b8afead0
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2023-12-12 11:47:55 +0600

    banner section added

--- src/components/Home/Navbar/Navbar.jsx
+++ src/components/Home/Navbar/Navbar.jsx
@@ -1,20 +1,30 @@
 import banner from '../../../assets/banner2.jpg'
-
+import { useTypewriter  } from 'react-simple-typewriter';
 const Navbar = () => {
+    const [typeEffect] = useTypewriter({
+        words: ['Web Developer', 'Learner', 'Student'],
+        loop: {},
+        typeSpeed: 100,
+        deleteSpeed: 40,
+      });
+    
+    
     return (
         <div>
+
            <div className="hero min-h-screen" style={{backgroundImage: `url(${banner})`}}>
   <div className="hero-overlay  bg-opacity-60"></div>
   <div className="hero-content text-center text-neutral-content">
     <div className="">
-      <h1 className="mb-5 text-5xl font-bold">I'am Mohammad Mashrafil Hossain Mahi</h1>
-      <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
+      <h1 id='typewriter' className="mb-5 text-5xl font-bold">I'am Mohammad Mashrafil Hossain Mahi</h1>
+      <p className="mb-5 text-5xl font-bold">I'am a <span className='text-orange-600 border-b-2 border-orange-600'>{typeEffect}</span></p>
+     
+      <button className="btn bg-orange-700 text-white text-lg font-bold py-2 px-4">Hire Me</button>
       
     </div>
   </div>
 </div> 
-
+   
         </div>
     );
 };

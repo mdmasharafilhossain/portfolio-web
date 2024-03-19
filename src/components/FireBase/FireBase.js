49daf896c66b414081b14ebf37e576b6ca209602
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2024-03-19 17:01:38 +0600

    favicon updated

--- src/components/FireBase/FireBase.js
+++ src/components/FireBase/FireBase.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBGqY68OV96QscCNvfttT5M3yZ0XL6cuFA",
   authDomain: "mashrafilhossain.firebaseapp.com",
@@ -13,6 +10,6 @@ const firebaseConfig = {
   appId: "1:501747203294:web:4e935ce043420bc126164f"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 export default app;

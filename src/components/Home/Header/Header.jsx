18e9cd74c99a420db26eecaa00b1d0ae9e6a80f9
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2024-03-31 11:37:29 +0600

    icon change on header section

--- src/components/Home/Header/Header.jsx
+++ src/components/Home/Header/Header.jsx
@@ -1,5 +1,6 @@
 import profile from '../../../assets/rsz_finalfjh.png'
 import logo from '../../../assets/Screenshot (1216).png'
+import { HiOutlineBars3 } from "react-icons/hi2";
 const Header = () => {
     const NavLinks = <>
     <li><a className='text-orange-700'
@@ -66,7 +67,7 @@ const Header = () => {
                     <div className="navbar-start">
                         <div className="dropdown">
                             <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                            <HiOutlineBars3 className='text-2xl'/>
                             </label>
                             <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                              {NavLinks}
